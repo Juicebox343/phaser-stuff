d44d3a7135098120be543cfa8c04e0f63ff2717d
Joe <47640432+Juicebox343@users.noreply.github.com>
2020-04-15 11:46:12 -0700

    setting up controls and world1

--- project1/src/CST.js
+++ project1/src/CST.js
@@ -1,25 +1,6 @@
 export const CST = {
     SCENES: {
         LOAD: "LOAD",
-        MENU: "MENUS",
-        CHARACTER: "CHARACTER",
-        INTRO: "INTRO",
-        WORLD1: "WORLD1"
-    },
-    IMAGE: {
-        CLOUDS_BG: "bg-clouds.png",
-        MOUNT_BG: "bg-mountains.png",
-        TREES_BG: "bg-trees.png",
-        BIRD: "robin_fly.png",
-        OPTIONS: "options.png",
-        RESUME: "resume_game.png",
-        NEW: "new_game.png",
-        TITLE: "homestead_title.png"
-    },
-    SPRITE: {
-        ROBIN: "robin_fly.png"
-    },
-    AUDIO: {
-        TITLE_MUSIC: "carefree-by-kevin-macleod-from-filmmusic-io.mp3"
+        MENU: "MENUS"
     }
 }
--- project1/src/main.js
+++ project1/src/main.js
@@ -1,17 +1,10 @@
 import {LoadScene} from "./scenes/LoadScene.js";
 import {MenuScene} from "./scenes/MenuScene.js";
-import {IntroScene} from "./scenes/IntroScene.js";
-import {CharacterScene} from "./scenes/CharacterScene.js";
-import {World1Scene} from "./scenes/World1Scene.js";
 
-let config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: [LoadScene, MenuScene, IntroScene, CharacterScene, World1Scene],
-    render: {
-        pixelArt: true
-    }
-}
-
-let game = new Phaser.Game(config);
+let game = new Phaser.Game({
+    width: 100,
+    height: 100,
+    scene: [
+        LoadScene, MenuScene
+    ]
+});
