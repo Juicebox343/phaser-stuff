9f385584b7f2f1f03966b3685345345cc7932941
Joe <47640432+Juicebox343@users.noreply.github.com>
2020-04-18 17:26:07 -0700

    fixed animation, begin stateEngine

--- project1/src/scenes/LoadScene.js
+++ project1/src/scenes/LoadScene.js
@@ -9,76 +9,10 @@ export class LoadScene extends Phaser.Scene{
     init(){
 
     }
-
-    loadImages(){
-        this.load.setPath("./dist/assets/images");
-        for(let prop in CST.IMAGE){
-            this.load.image(CST.IMAGE[prop], CST.IMAGE[prop]);
-        }
-    }
-
-    loadAudio(){
-        this.load.setPath("./dist/assets/audio");
-        for(let prop in CST.AUDIO){
-            this.load.audio(CST.AUDIO[prop], CST.AUDIO[prop]);
-        }
-    }
-
-    loadSprites(frameConfig){
-        this.load.setPath("./dist/assets/sprites");
-        for(let prop in CST.SPRITE){
-            this.load.spritesheet(CST.SPRITE[prop], CST.SPRITE[prop], frameConfig);
-        }
-    }
-
-    loadCharacters(frameConfig){
-        this.load.setPath("./dist/assets/sprites");
-        for(let prop in CST.CHARACTERS){
-            this.load.spritesheet(CST.CHARACTERS[prop], CST.CHARACTERS[prop], frameConfig);
-        }
-    }    
-
     preload(){
-
-
-        this.loadImages();
-        this.loadAudio();
-        this.loadSprites({
-            frameHeight: 32,
-            frameWidth: 32
-        });
-        this.loadCharacters({
-            frameHeight: 64,
-            frameWidth: 64
-        });
-
-      
-
-        //loading bar
-        let loadingBar = this.add.graphics({
-            fillStyle: {
-                color: 0xffffff
-            }
-        })
-
-        //loading events
-
-        this.load.on("progress", (percent) => {
-            loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 25);            
-            // console.log(`loading: ${percent * 100}%`);
-        })
-
-        this.load.on('complete', ()=>{
-            // this.scene.start(CST.SCENES.MENU);
-        })
-
-        this.load.on("load", (file) =>{
-            // console.log(file.src);
-        })
-
 
     }
     create(){
-        this.scene.start(CST.SCENES.MENU);
+        this.scene.start(CST.SCENES.MENU, "Hello from load Scene")
     }
 }
